fb3ee08490171c240dfa9e17bf13b094ea6c1dd6
Byo <alerhandrojp@gmail.com>
2022-11-17 00:29:34 -0400

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Valores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("digite um valor");
+
+            int Valor = Convert.ToInt32(Console.ReadLine());
+
+            if (Valor >= 20)
+            {
+                Console.WriteLine("maior que vinte ");
+            }
+            else
+            {
+                Console.WriteLine("menor que vinte");
+            }
+
+
+
+        }
+    }
+}
